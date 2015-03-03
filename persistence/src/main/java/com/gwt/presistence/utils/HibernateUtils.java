7368f82864bbf614f516a2469ddb407a2ac5c010
houk1987 <houk1987@gmail.com>
2015-03-03 19:27:58 +0800

    使用了hibernate 3

--- persistence/src/main/java/com/gwt/presistence/utils/HibernateUtils.java
+++ persistence/src/main/java/com/gwt/presistence/utils/HibernateUtils.java
@@ -1,39 +1,49 @@
 package com.gwt.presistence.utils;
 
+import com.gwt.presistence.entity.Ofuser;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
+ *
  * Created by 30 on 2015/2/15.
  */
 public class HibernateUtils {
-
     private static HibernateUtils hibernateUtils;
-
-    private Configuration configuration;
 
     private SessionFactory sessionFactory;
 
-
     public static HibernateUtils getInstance() {
-        if (hibernateUtils == null){
+        if (hibernateUtils == null) {
             hibernateUtils = new HibernateUtils();
         }
         return hibernateUtils;
     }
 
     private HibernateUtils() {
-        this.configuration = new Configuration();
-        StandardServiceRegistryBuilder srb = new  StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-        StandardServiceRegistry sr = srb.build();
-        this.sessionFactory = configuration.buildSessionFactory(sr);
+        sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
     }
 
-    public Session getSession(){
+    public Session getSession() {
         return sessionFactory.openSession();
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void main(String[] args) {
+        Session session = HibernateUtils.getInstance().getSession();
+        Transaction tx = session.getTransaction();
+        tx.begin();
+        Ofuser ofuser = new Ofuser();
+        ofuser.setUsername("123123213123");
+        ofuser.setCreationDate("a");
+        ofuser.setModificationDate("1");
+        session.save(ofuser);
+        tx.commit();
     }
 
 }
--- persistence/src/test/java/com/gwt/presistence/utils/HibernateUtilsTest.java
+++ persistence/src/test/java/com/gwt/presistence/utils/HibernateUtilsTest.java
@@ -1,14 +1,16 @@
 package com.gwt.presistence.utils;
 
+import com.gwt.presistence.Ofuserentity;
 import org.hibernate.Session;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class HibernateUtilsTest {
 
     @Test
     public void testGetSession() throws Exception {
         Session session = HibernateUtils.getInstance().getSession();
+        Ofuserentity ofuserentity = new Ofuserentity();
+        ofuserentity.setUsername("1");
+        session.delete(ofuserentity);
     }
 }
